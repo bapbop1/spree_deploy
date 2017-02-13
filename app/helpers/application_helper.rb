2f3b7392a4868104385ed8e230dc014c29c3d10c
Duong Doan <duongdn@nustechnology.com>
2017-02-13 16:59:40 +0700

    Update: Image tracking for sale transaction

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+  def mt_img_tracking
+    if cookies[:current_visitor_id]
+      href = "http://localhost:3000/sys/sale.js"
+      productInfo = []
+      @order.line_items.each do |item|
+        productInfo.push({
+          product_id: item.variant.product.id.to_s,
+          product_price: item.price.to_s,
+          product_quantity: item.quantity.to_s,
+          product_name: item.variant.product.name
+        });
+      end
+
+      sale = {
+        sale: {
+          visitor_id: cookies[:current_visitor_id],
+          order_id: @order.number,
+          product_info: URI.escape(productInfo.to_json),
+          referer_url: URI.escape(cookies[:referer_url])
+        }
+      }
+      href+= "?" + sale.to_query
+      image_tag href, width: 1, height: 1, alt: ""
+
+    end
+  end
 end

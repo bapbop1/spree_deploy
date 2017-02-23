7cfc830a8e52f9a90dd086ad2f75adaf4fe1c834
hath <hath@nustechnology.com>
2017-02-23 15:52:40 +0700

    test

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,3 +1,3 @@
-TRACTJS = 'https://app.monetrackdev.com/track.maximize.js'
-SALE_URL = 'https://app.monetrackdev.com/sys/sale.js'
-LEAD_URL ='https://app.monetrackdev.com/sys/lead.js'
+TRACTJS = 'https://dev.monetrack.com/track.maximize.js'
+SALE_URL = 'https://dev.monetrack.com/sys/sale.js'
+LEAD_URL ='https://dev.monetrack.com/sys/lead.js'
